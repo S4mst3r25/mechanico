6f9969dc78a42b536100acd276389c2f46fb023a
S4mst3r25 <77197894+S4mst3r25@users.noreply.github.com>
2024-01-07 17:18:18 +0200

    Make base layout
    Co-authored-by: TheHat1 <TheHat1@users.noreply.github.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,17 @@
 <template>
-    <h1>Welcome to Mechanico!</h1>
+    <div class="text-center mt-20">
+        <h1 class="font-bold text-4xl mb-4">Welcome to Mechanico!</h1>
+        <p class="font-bold text-xl mb-4">A community-powered library of interactive 3D mechanisms.</p>
+        <button  @click="goToLibrary()" class="bg-zinc-700 p-2 rounded-md text-white font-bold hover:bg-zinc-800 transition">Get started</button>
+    </div>
 </template>
 
 <script setup>
+const router = useRouter()
+
+function goToLibrary() {
+    router.push("/library")
+}
 </script>
 
 <style scoped>
---
+++ pages/library/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Library</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
