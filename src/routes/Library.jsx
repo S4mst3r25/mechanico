8b091bf26bf1d9a3a0dc1b479e4e5a50441c1ae0
TheHat1 <152319491+TheHat1@users.noreply.github.com>
2024-01-21 15:33:59 +0200

    Fix text margin

--- src/routes/Library.jsx
+++ src/routes/Library.jsx
@@ -4,7 +4,7 @@ import differential from '../assets/images/thumbnails/differential.png'
 export default function Library() {
     return (
         <>
-            <h1 className="font-bold text-2xl mt-10 text-center">Models library</h1>
+            <h1 className="font-bold text-3xl mt-36 text-center">Models library</h1>
             <div className="m-auto">
                 <div className="flex flex-wrap mt-16 gap-4 justify-center">
                     <Card img={differential} name="Differential" />
