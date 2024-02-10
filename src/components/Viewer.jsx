288c93d07a78f4dfc2d93d8586c0e9cd4fc885d4
S4mst3r25 <77197894+S4mst3r25@users.noreply.github.com>
2024-02-10 21:40:06 +0200

    Set canvas to fullscreen & improve scene lights
    Co-authored-by: TheHat1 <TheHat1@users.noreply.github.com>

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,11 +1,11 @@
-import { Canvas } from '@react-three/fiber'
-import { Suspense, useEffect } from "react"
-import { Environment, OrbitControls, useAnimations, useGLTF } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Suspense, useEffect, useRef } from "react"
+import { OrbitControls, Stats, useAnimations, useGLTF } from '@react-three/drei'
 
 function Model({ path }) {
     const { scene, animations } = useGLTF(path)
     const { actions, names } = useAnimations(animations, scene)
-   
+
     useEffect(() => {
         actions[names[0]].play()
     }, [])
@@ -21,12 +21,13 @@ export default function Viewer({ modelPath }) {
 
     return (
         <>
-            <div className="size-full bg-gray-600">
+            <div className="w-full h-[100vh] bg-zinc-400">
                 <Canvas camera={{ fov: 35, zoom: 5, near: 1, far: 1000 }}>
                     <Suspense fallback={null}>
                         <Model path={modelPath} />
                     </Suspense>
-                    <Environment preset="sunset" />
+                    <ambientLight />
+                    <directionalLight position={[1, 20, 1]} />
                     <OrbitControls />
                 </Canvas>
             </div>
