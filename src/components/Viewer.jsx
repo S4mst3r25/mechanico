8457c7515ea477669c8607b324dfead2d747eb5d
S4mst3r25 <77197894+S4mst3r25@users.noreply.github.com>
2024-02-13 08:47:38 +0200

    Center model & update TODO

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,6 +1,6 @@
 import { Canvas } from '@react-three/fiber'
 import { Suspense, useEffect } from "react"
-import { Grid, Html, OrbitControls, useAnimations, useGLTF, useProgress } from '@react-three/drei'
+import { Center, Grid, Html, OrbitControls, useAnimations, useGLTF, useProgress } from '@react-three/drei'
 
 function Model({ path }) {
     const { scene, animations } = useGLTF(path)
@@ -12,16 +12,19 @@ function Model({ path }) {
 
     return (
         <>
-            <primitive object={scene} />
+            <Center>
+                <primitive object={scene} scale={[8, 8, 8]} />
+            </Center>
         </>
+
     )
 }
 
-function Loader(){
+function Loader() {
     const { progress } = useProgress();
     return (
         <>
-        <Html center className="font-bold text-2xl text-white whitespace-nowrap">Loading: {progress} %</Html>
+            <Html center className="font-bold text-2xl text-white whitespace-nowrap">Loading: {progress} %</Html>
         </>
     )
 }
@@ -31,15 +34,14 @@ export default function Viewer({ modelPath }) {
     return (
         <>
             <div className="w-full h-[100vh] bg-zinc-600">
-                <Canvas camera={{ fov: 35, zoom: 5, near: 1, far: 1000 }}>
-                    <Suspense fallback={<Loader/>}>
+                <Canvas>
+                    <Suspense fallback={<Loader />}>
                         <Model path={modelPath} />
                     </Suspense>
                     <ambientLight />
                     <directionalLight position={[1, 20, 1]} />
-                    <Grid args={[5,5]}/>
-                    <axesHelper args={[5]}/>
-                    <OrbitControls />
+                    <OrbitControls makeDefault={true} />
+
                 </Canvas>
             </div>
 
