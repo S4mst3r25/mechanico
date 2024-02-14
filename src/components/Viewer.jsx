e3d81ed0bdcc8fb93e9e179c03186a4370edb913
TheHat1 <152319491+TheHat1@users.noreply.github.com>
2024-02-14 18:20:38 +0200

    Add playback speed slider

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,9 +1,9 @@
 import { Canvas, useThree } from '@react-three/fiber'
-import { Suspense, useEffect } from "react"
+import { Suspense, useEffect, useState } from "react"
 import { Center, Html, OrbitControls, useAnimations, useGLTF, useProgress } from '@react-three/drei'
 import ReactSlider from "react-slider"
 
-function Model({ path }) {
+function Model({ path , sliderValue }) {
     const { scene, animations } = useGLTF(path)
     const { actions, names } = useAnimations(animations, scene)
 
@@ -12,8 +12,8 @@ function Model({ path }) {
     state.camera.position.set(1, 3, 2)
 
     useEffect(() => {
-        actions[names[0]].setEffectiveTimeScale(0.4).play()
-    }, [])
+        actions[names[0]].setEffectiveTimeScale(sliderValue).play()
+    }, [sliderValue])
 
     return (
         <>
@@ -35,21 +35,27 @@ function Loader() {
 }
 
 export default function Viewer({ modelPath }) {
+        let [value, setValue] = useState(1)
+        
     return (
         <>
-            <div>
+            <div className='top-[100px] left-[20px] absolute'>
+                <h1 className='font-bold text-xl text-white whitespace-nowrap'>Playback speed</h1>
                 <ReactSlider
-                    className='left-[20px] top-[100px] absolute z-10 w-[150px] h-[10px] bg-zinc-700 rounded-lg'
+                    className='top-[8px] z-10 w-[150px] h-[10px] bg-zinc-700 rounded-lg'
                     trackClassName=''
                     thumbClassName='top-[-2.5px] w-[15px] h-[15px] bg-zinc-900 rounded-lg'
                     min={0}
                     max={100}
+                    defaultValue={0}
+                    value={value * 100}
+                    onChange={(value) => setValue(value / 100)}
                 />
             </div>
             <div className="w-full h-[100vh] bg-zinc-600">
                 <Canvas>
                     <Suspense fallback={<Loader />}>
-                        <Model path={modelPath} />
+                        <Model path={modelPath} sliderValue={value}/>
                     </Suspense>
                     <ambientLight />
                     <directionalLight position={[1, 20, 1]} />
