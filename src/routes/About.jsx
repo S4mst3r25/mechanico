259bbffa6831ee8c84618f5b2946b75960760f95
TheHat1 <152319491+TheHat1@users.noreply.github.com>
2024-02-14 16:30:18 +0200

    Add layout to about page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,7 +1,35 @@
 export default function About() {
     return (
-        <>
-            <h1>About</h1>
-        </>
+        <div className="mt-[100px] mx-8 content-center">
+            <h1 className="font-bold text-4xl mb-4 text-center">About us</h1>
+            <p className="text-center text-xl mb-4">Mechanico is a platform designed to bring mechanical designs to life,
+                offering engineers, designers, and enthusiasts a better way of
+                understanding mechanisms and mechanical principles.
+            </p>
+            <img src="src/assets/images/thumbnails/differential.png"/>
+            <p className="text-center text-xl mb-4">
+            What sets Mechanico apart is our commitment to simplicity without 
+            compromising on sophistication. Whether you're a seasoned professional 
+            or a curious beginner, our user-friendly interface ensures that you can
+             effortlessly navigate complex designs, delve into intricate details, and 
+             unleash your creativity without any technical barriers.
+            </p>
+            <img src="src/assets/images/thumbnails/differential.png"/>
+            <p className="text-center text-xl mb-4">
+            Behind Mechanico is a team of passionate developers dedicated 
+            to pushing the boundaries of innovation. We're driven by a shared 
+            vision of giving free access to powerful visualization tools, 
+            empowering creators of all backgrounds to turn their dreams into 
+            reality.
+            </p>
+            <img src="src/assets/images/thumbnails/differential.png"/>
+            <p className="text-center text-xl mb-4">
+            Join us on this exciting journey as we revolutionize the way we 
+            visualize and interact with mechanical systems. Whether you're an
+             inventor, a designer, or simply someone fascinated by the world 
+             of mechanics, Mechanico is your gateway to a universe of endless
+            possibilities.
+            </p>
+        </div>
     )
 }
