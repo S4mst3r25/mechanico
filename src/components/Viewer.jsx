cf7e9bb4b655a38d8161cc864180aa60033ecde7
S4mst3r25 <77197894+S4mst3r25@users.noreply.github.com>
2024-02-14 10:16:21 +0000

    Set initial camera position & add react-slider library

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,10 +1,15 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import { Suspense, useEffect } from "react"
 import { Center, Html, OrbitControls, useAnimations, useGLTF, useProgress } from '@react-three/drei'
+import ReactSlider from "react-slider"
 
 function Model({ path }) {
     const { scene, animations } = useGLTF(path)
     const { actions, names } = useAnimations(animations, scene)
+
+    const state = useThree()
+
+    state.camera.position.set(1,3,2)
 
     useEffect(() => {
         actions[names[0]].play()
@@ -33,6 +38,7 @@ export default function Viewer({ modelPath }) {
     return (
         <>
             <div className="w-full h-[100vh] bg-zinc-600">
+                <ReactSlider/>
                 <Canvas>
                     <Suspense fallback={<Loader />}>
                         <Model path={modelPath} />
