6ac1f185848d3c88720b1ab841b5a91baa85790f
S4mst3r25 <77197894+S4mst3r25@users.noreply.github.com>
2024-02-15 22:02:06 +0200

    Update content

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,8 +1,8 @@
 export default function About() {
     return (
-        <div className="mt-[100px] mx-8 content-center text-center">
+        <div className="mt-32 max-w-[900px] mx-auto text-center">
             <h1 className="font-bold text-4xl mb-4">About Mechanico</h1>
-            <p className="text-xl mb-4">
+            <p className="text-xl mt-16">
                 Mechanico is a library of interactive 3D models of mechanical contraptions and systems.
                 It's a beginner-friendly platform that helps engineering students, hobbyists and curious people understand mechanical systems better.
             </p>
