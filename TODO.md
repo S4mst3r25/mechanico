411c765477e700e28313ef66e49311daebcf4fb1
S4mst3r25 <77197894+S4mst3r25@users.noreply.github.com>
2024-02-28 19:00:03 +0200

    Make card size fixed (independent)  & update TODO

--- TODO.md
+++ TODO.md
@@ -6,18 +6,17 @@ Project Description
 
 ### Todo
 
-- [ ] Add section view feature  
 - [ ] Add 3d object/s to homepage for decoration & improve overall look  
-- [ ] Create better lighting setup  
-- [ ] Display mechanism name somewhere in ModelView/Viewer  
 - [ ] Throw not found error if model id parameter is wrong  
+- [ ] Display mechanism name somewhere in ModelView/Viewer  
 - [ ] Add thumbnails as a part of the models.json file  
+- [ ] Add section view feature  
 - [ ] Add information text tooltips to model parts  
 - [ ] Code clean-up & restructuring  
 
 ### In Progress
 
-- [ ] Card size changes depending on image size  
+- [ ] Create better lighting setup  
 
 ### Done ✓
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,7 @@ export default function Card({ img, name, modelId }) {
         <>
             <div onClick={navigateToModelPage} className="flex flex-col items-center shadow-md shadow-gray-500 w-72 max-h-[300px] border-[1px] border-zinc-800 cursor-pointer  hover:shadow-gray-700 hover:opacity-80 transition group">
                 <button className="mt-[110px] hidden group-hover:block absolute bg-zinc-500 text-white p-2 font-bold transition-opacity">View model</button>
-                <img className="max-h-full" src={ img }/>
+                <img className="h-[218px]" src={ img }/>
                 <div className="p-3 h-20 bg-zinc-800 w-full">
                     <p className="text-lg font-bold line-clamp-2 text-white">{ name }</p>
                 </div>
