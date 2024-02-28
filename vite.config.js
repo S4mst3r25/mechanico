dd07d35ab0e4843c79b0793d9f8776b0d8b43f15
S4mst3r25 <77197894+S4mst3r25@users.noreply.github.com>
2024-02-28 20:51:50 +0200

    Add vsharp (vite plugin) to compress assets in production build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vsharp from 'vite-plugin-vsharp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vsharp(),
+  ],
   server: {
     port: 3000
   },
